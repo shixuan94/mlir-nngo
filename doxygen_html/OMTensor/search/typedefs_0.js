7cea355dda99786e5db1bab86786855c522f55f9
Doxygen Bot (GitHub Action) <onnxmlir@onnx.ai>
2022-02-17 11:19:17 +0000

    Push docs and doxygen updates into gh-pages branch

--- doxygen_html/OMTensor/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['omtensor_37',['OMTensor',['../_o_m_tensor_8h.html#a7099d0375d9023e04c7fe5b651ee079e',1,'OMTensor.h']]]
-];
--- doxygen_html/OMTensorList/search/searchdata.js
+++ doxygen_html/OMTensorList/search/searchdata.js
@@ -1,24 +1,18 @@
 var indexSectionsWithContent =
 {
   0: "o",
-  1: "o",
-  2: "o",
-  3: "o"
+  1: "o"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files",
-  2: "functions",
-  3: "typedefs"
+  1: "files"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files",
-  2: "Functions",
-  3: "Typedefs"
+  1: "Files"
 };
 
